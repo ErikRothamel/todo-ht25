e3adfcf74eca375888f61cb8667ea11a661c5e7f
ErikRothamel <rothamel.erik@gmail.com>
2025-11-20 16:30:53 +0100

    arbete

--- app.rb
+++ app.rb
@@ -8,8 +8,29 @@ require 'sinatra/reloader'
 
 
 # Routen /
-get '/' do
+get '/todo' do
+    
+    db = SQLite3::Database.new("db/todos.db")
+
+    db.results_as_hash = true
+
+ 
+    @tododata = db.execute("SELECT * FROM todos")
+
+
+
     slim(:index)
+end
+
+post('/todo') do
+  name = params[:name]
+  description = params[:description]
+
+  db = SQLite3::Database.new("db/todos.db")
+
+  db.execute("INSERT INTO todos (name, description) VALUES (?, ?)", [name, description])
+
+  redirect '/todo'
 end
 
 
