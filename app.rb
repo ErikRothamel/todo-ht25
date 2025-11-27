b228b5b4398658cdb37c8f6fb8b661a0bae5b8b5
ErikRothamel <rothamel.erik@gmail.com>
2025-11-27 16:43:12 +0100

    Arbete 2

--- app.rb
+++ app.rb
@@ -33,4 +33,33 @@ post('/todo') do
   redirect '/todo'
 end
 
+post ('/todos/:id/delete') do
+
+  id = params[:id].to_i
+
+  db = SQLite3::Database.new("db/todos.db")
+  db.execute("DELETE FROM todos WHERE id = ?",id)
+
+  redirect('/todo')
+end
+
+get('/todos/:id/edit') do
+  
+  db = SQLite3::Database.new("db/todos.db")
+  db.results_as_hash = true
+  id = params[:id].to_i
+  @tododata = db.execute("SELECT * FROM todos WHERE id = ?",id).first
+  slim(:edit)
+end
+
+post('/todos/:id/update') do
+  id = params[:id].to_i
+  name = params[:name]
+  description = params[:description]
+
+  db = SQLite3::Database.new("db/todos.db")
+  db.execute("UPDATE todos SET name=?,description=? WHERE id=?",[name,description,id])
+
+  redirect('/todo')
+end
 
